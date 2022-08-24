202a458dd83afdc40c65cc36c5442b987e5b9c26
Andrew S <andrewsales2004@gmail.com>
2022-08-24 17:39:12 -0500

    Began work on city name and weather and put initial variables for Open Weather API query

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 var test = function() {
     console.log("function was called")
 }
+
+var currentURL = "https://api.openweathermap.org/data/2.5/weather?";
+        var cityName = city;
+        var unitsURL = "&units=imperial";
+        var apiIdURL = "&appid="
+        var apiKey = "2201c91e6308abe919670594e9f740c3";
+        var openCurrWeatherAPI = currentURL + cityName + unitsURL + apiIdURL + apiKey;
