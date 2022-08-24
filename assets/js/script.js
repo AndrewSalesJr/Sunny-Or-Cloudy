5d6e9f3fd073c8911dccbbb515c250589c88c340
Andrew S <andrewsales2004@gmail.com>
2022-08-24 17:43:41 -0500

    completed html portion by adding forecast div

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,19 @@ var currentURL = "https://api.openweathermap.org/data/2.5/weather?";
         var apiIdURL = "&appid="
         var apiKey = "2201c91e6308abe919670594e9f740c3";
         var openCurrWeatherAPI = currentURL + cityName + unitsURL + apiIdURL + apiKey;
+
+        $.ajax({
+            url: openCurrWeatherAPI,
+            method: "GET"
+        }).then(function (response1) {
+
+        weatherObj = {
+            city: `${response1.name}`,
+            wind: response1.wind.speed,
+            humidity: response1.main.humidity,
+            temp: Math.round(response1.main.temp),
+
+            date: (convertDate(response1.dt)),
+            icon: `http://openweathermap.org/img/w/${response1.weather[0].icon}.png`,
+            desc: response1.weather[0].description
+        }
